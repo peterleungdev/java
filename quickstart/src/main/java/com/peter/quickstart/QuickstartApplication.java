4643e7de973bc1923c01a6766c0951f500f4fcf4
Peter Leung <peterleungkw99@gmail.com>
2024-03-14 22:10:05 +0800

    add: spring app setting

--- quickstart/src/main/java/com/peter/quickstart/QuickstartApplication.java
+++ quickstart/src/main/java/com/peter/quickstart/QuickstartApplication.java
@@ -1,5 +1,6 @@
 package com.peter.quickstart;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickstartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuickstartApplication.class, args);
+		SpringApplication app = new SpringApplication(QuickstartApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
-
 }
